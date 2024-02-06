340db5ed860e275f4033b8d749fb4805cc1b1a91
proninmo <114398335+proninmo@users.noreply.github.com>
2024-02-06 10:35:56 +0300

    Update InitFromFiles.groovy

--- src/ru/pulsar/jenkins/library/steps/InitFromFiles.groovy
+++ src/ru/pulsar/jenkins/library/steps/InitFromFiles.groovy
@@ -43,7 +43,7 @@ class InitFromFiles implements Serializable {
 
         Logger.println("Выполнение загрузки конфигурации из файлов")
         String vrunnerPath = VRunner.getVRunnerPath();
-        def initCommand = "$vrunnerPath init-dev --src $srcDir --ibconnection \"/F./build/ib\""
+        def initCommand = "$vrunnerPath init-dev --src $srcDir"
         VRunner.exec(initCommand)
     }
 }
